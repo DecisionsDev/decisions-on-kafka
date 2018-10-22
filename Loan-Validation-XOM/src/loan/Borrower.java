54176e1e9fea2d3940bee8cce56dde194634bd1e
Moussa Yattara <myattara@fr.ibm.com>
2018-10-22 16:55:07 +0200

    Add Borrower

--- Loan-Validation-XOM/src/loan/Borrower.java
+++ Loan-Validation-XOM/src/loan/Borrower.java
@@ -1,5 +1,22 @@
 package loan;
 
-public class Borrower {
+import java.util.Calendar;
 
+public class Borrower implements java.io.Serializable{
+
+	
+	private String firstName;
+	private String lastName;
+	private Calendar birth;
+	private SSN SSN;
+	private int yearlyIncome;
+	private String zipCode;
+	private int creditScore;
+	private Borrower spouse;
+
+	public class SSN implements java.io.Serializable{
+		
+	}
 }
+
+
