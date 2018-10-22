f324b797839604c9c367ede289b90fda863f1608
Moussa Yattara <myattara@fr.ibm.com>
2018-10-22 15:24:32 +0200

    Add ODM J2SEE project

---
+++ ODMJ2SEclient/src/main/java/odm/ds/kafka/ODMJ2SEclient/Main.java
@@ -0,0 +1,13 @@
+package odm.ds.kafka.ODMJ2SEclient;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
