75af690f45a068197b501a8b17ab5ce6995c3056
Moussa Yattara <myattara@fr.ibm.com>
2018-10-22 14:59:43 +0200

    Change project structure

---
+++ ODM-Kafka/Readme.md
@@ -0,0 +1,45 @@
+# ODM Decision Server with kafka
+[![Build Status](https://travis.ibm.com/MYattara/ODM-DecisionServer-Kafka.svg?token=YUDWXbAcjsyzHsqNF4a8&branch=master)](https://travis.ibm.com/MYattara/ODM-DecisionServer-Kafka)
+[![GitHub last commit (branch)](https://img.shields.io/github/last-commit/ODMDev/odm-ondocker/dev.svg)](https://github.ibm.com/MYattara/ODM-DecisionServer-Kafka)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+## Features
+
+This sample show how to use IBM ODM with Kafka
+
+## Requirments
+
+* Kafka
+* IBM ODM
+* Maven
+
+
+## Dependencies
+- Compile time
+- Test
+
+## Deployment
+* Clone the repository from github.
+`$ git clone --branch=master git@github.ibm.com:MYattara/ODM-DecisionServer-Kafka.git MYattara/ODM-DecisionServer-Kafka`
+* Run the project locally.
+`$ mvn clean install -Dmessage="yourMessage" -DtopicName="topicName" -Dserverurl="yourkafkahost:9092" -Dgroupid="ConsumerGroupid"`
+* To run the main class SampleMain :$ `mvn exec:java -Dexec.mainClass="odm.ds.kafka.main.SampleMain" -Dexec.args="yourkafkahost:port topicname message" -Dmaven.test.skip=true`
+* To run the integration test : `$ mvn -Dtest=SampleTest -Dmessage="yourMessage" -DtopicName="topicName" -Dserverurl="yourkafkahost:port" test`
+
+## Contributing
+
+## References
+* [IBM Operational Decision Manager Developer Center](https://developer.ibm.com/odm/)
+* [Java EE rule session](https://www.ibm.com/support/knowledgecenter/en/SSQP76_8.9.2/com.ibm.odm.dserver.rules.samples/res_smp_topics/smp_res_javaee.html)
+
+## Issues and contributions
+
+## License
+[Apache 2.0](LICENSE)
+## Notice
+© Copyright IBM Corporation 2018.
+
+[![Build Status](https://travis.ibm.com/MYattara/ODM-DecisionServer-Kafka.svg?token=YUDWXbAcjsyzHsqNF4a8&branch=master)](https://travis.ibm.com/MYattara/ODM-DecisionServer-Kafka)
+[![GitHub last commit (branch)](https://img.shields.io/github/last-commit/ODMDev/odm-ondocker/dev.svg)](https://github.ibm.com/MYattara/ODM-DecisionServer-Kafka)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
---
+++ ODM-Kafka/src/main/java/odm/ds/kafka/producer/SampleProducer.java
@@ -0,0 +1,77 @@
+/*
+ *
+ *   Copyright IBM Corp. 2018
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+package odm.ds.kafka.producer;
+
+import java.util.Locale;
+import java.util.Properties;
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class SampleProducer {
+	final Logger mylogger=Logger.getLogger(SampleProducer.class.getName());
+	ResourceBundle mybundle=ResourceBundle.getBundle("MessagesBundle");
+	
+	// Create an instance of Producer
+	public Producer<String, String> producerInstance (String serverurl,int numberparam) 
+	{
+		mylogger.info("Current Locale: " + Locale.getDefault());
+		if(numberparam==0){
+			mylogger.severe(mybundle.getString("wrong_topic"));
+			return null;
+		}
+		String server=serverurl;
+		Properties props=new Properties();
+		props.put("bootstrap.servers", server);
+		props.put("acks", "all");
+		props.put("retries", 0);
+		props.put("batch.size", 16384);
+		props.put("linger.ms", 1);
+		props.put("buffer.memory", 33554432);
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("PARTITIONER_CLASS_CONFIG",DataPartitioner.class.getCanonicalName());
+		props.put("partitions.0", "partition 0");
+		props.put("partitions.1", "partition 1");
+		Producer<String,String> producer=new KafkaProducer<String,String>(props);
+		return producer;
+	}
+	
+	// Send a message the iterrative message to a topic
+	public void sendmessage(Producer<String, String> producer,String topicname){
+		String topicName=topicname;
+		for(int i=0;i<6;i++)
+			producer.send(new ProducerRecord<String,String>(topicName,Integer.toString(i),Integer.toString(i)));
+		mylogger.info(mybundle.getString("notif_sent"));
+		producer.close();
+		
+	}
+	
+	// Send a String message to a topic
+	public void sendmessageString(Producer<String, String> producer,String topicname,String message){
+		String topicName=topicname;
+		producer.send(new ProducerRecord<String,String>(topicName,"2",message));
+		mylogger.info(mybundle.getString("notif_sent"));
+		producer.close();
+		
+	}
+
+}
