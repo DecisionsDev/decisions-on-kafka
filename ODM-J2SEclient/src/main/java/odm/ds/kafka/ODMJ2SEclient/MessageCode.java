1436e922d13d2d544453ffa23b57970b394546e2
Moussa Yattara <myattara@fr.ibm.com>
2018-10-24 14:21:19 +0200

    Add MessageCode

---
+++ ODM-J2SEclient/src/main/java/odm/ds/kafka/ODMJ2SEclient/MessageCode.java
@@ -0,0 +1,52 @@
+/*
+ *
+ *   Copyright IBM Corp. 2018
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+
+package odm.ds.kafka.ODMJ2SEclient;
+
+class MessageCode {
+
+    static final String RULEAPP_FILE_NOT_FOUND = "RULEAPP_FILE_NOT_FOUND"; // No_i18n
+
+    static final String RULEAPP_CLASSLOADER_RESOURCE_NOT_FOUND = "RULEAPP_CLASSLOADER_RESOURCE_NOT_FOUND"; // No_i18n
+
+    static final String RULEAPP_PROCESSED = "RULEAPP_PROCESSED"; // No_i18n
+
+    static final String RULEAPP_NOT_PROCESSED = "RULEAPP_NOT_PROCESSED"; // No_i18n
+
+    static final String EMPTY_RULEAPP = "EMPTY_RULEAPP"; // No_i18n
+
+    static final String RULESETS_ADDED = "RULESETS_ADDED"; // No_i18n
+
+    static final String RULESET_ADDED = "RULESET_ADDED"; // No_i18n
+
+    static final String SAMPLE_ERROR_INVALID_RULESET_PATH = "SAMPLE_ERROR_INVALID_RULESET_PATH"; // No_i18n
+
+    static final String SAMPLE_RULESET_PATH = "SAMPLE_RULESET_PATH"; // No_i18n
+
+    static final String SAMPLE_ERROR_MISSING_RULESET_PATH = "SAMPLE_ERROR_MISSING_RULESET_PATH"; // No_i18n
+
+    static final String SAMPLE_RULEAPP = "SAMPLE_RULEAPP"; // No_i18n
+
+    static final String SAMPLE_USAGE_PREFIX = "SAMPLE_USAGE_PREFIX"; // No_i18n
+    
+    static final String SAMPLE_FOOTER_TAB = "SAMPLE_FOOTER_TAB"; // No_i18n
+
+    static final String SAMPLE_FOOTER = "SAMPLE_FOOTER"; // No_i18n
+
+    static final String SAMPLE_RULEAPP_DESCRIPTION = "SAMPLE_RULEAPP_DESCRIPTION"; // No_i18n
+}
