e00a64510f4e0ebe0fa6e85105212a32996bbca6
Moussa Yattara <myattara@fr.ibm.com>
2018-10-31 11:59:53 +0100

    Refactor the ClientApplication

--- ODM-J2SEclient/src/main/java/odm/ds/kafka/odmj2seclient/BusinessApplication.java
+++ ODM-J2SEclient/src/main/java/odm/ds/kafka/odmj2seclient/BusinessApplication.java
@@ -2,4 +2,7 @@ package odm.ds.kafka.odmj2seclient;
 
 public class BusinessApplication {
 
+	// Create a Consumer on topic Rq
+	// Execute the rulesetPath
+	// Create a Producer on topic Rp
 }
--- ODM-J2SEclient/src/main/java/odm/ds/kafka/odmj2seclient/ClientApplication.java
+++ ODM-J2SEclient/src/main/java/odm/ds/kafka/odmj2seclient/ClientApplication.java
@@ -1,5 +1,25 @@
 package odm.ds.kafka.odmj2seclient;
 
+import odm.ds.kafka.consumer.SampleConsumer;
+import odm.ds.kafka.producer.SampleProducer;
+
 public class ClientApplication {
+	
+	/**
+	 * Create a Producer on topic Rq, Create a Consumer on topic Rp 
+	 * @param serverurl
+	 * @param numberparam
+	 * @param topicNameRq
+	 * @param message
+	 * @param consumergroup
+	 * @param topicNameRp
+	 * 
+	 */
+	public static void setUpClientApp(String serverurl, int numberparam, String topicNameRq, String message, String consumergroup, String topicNameRp) {
+		SampleProducer myProducer=new SampleProducer();
+		myProducer.sendmessageString(myProducer.producerInstance(serverurl, numberparam), topicNameRq, message);
+		SampleConsumer myConsumer=new SampleConsumer();
+		myConsumer.consumeMessage(myConsumer.consumerInstance(serverurl, numberparam, consumergroup), topicNameRp);
+	}
 
 }
